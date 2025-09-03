891647ab4e12f89b402d368db2b03956e7cd3a4b
김승섭 <sub9707@naver.com>
2025-09-04 00:31:32 +0900

    appkey type error 수정

--- src/app/contact/layout.tsx
+++ src/app/contact/layout.tsx
@@ -1,6 +1,6 @@
 import Script from 'next/script'
 import React from 'react'
-export const API = `//dapi.kakao.com/v2/maps/sdk.js?appkey=567ff71073ba18f77afa29f39b180e6a&libraries=services,clusterer&autoload=false`
+export const API = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.KAKAOJSKEY}&libraries=services,clusterer&autoload=false`
 
 function Layout({ children }: { children: React.ReactNode }) {
     return (
--- src/components/ui/useKakaoLoader.tsx
+++ src/components/ui/useKakaoLoader.tsx
@@ -2,7 +2,7 @@ import { useKakaoLoader as useKakaoLoaderOrigin } from "react-kakao-maps-sdk"
 
 export default function useKakaoLoader() {
   useKakaoLoaderOrigin({
-    appkey: process.env.KAKAOJSKEY,
+    appkey: process.env.KAKAOJSKEY!,
     libraries: ["clusterer", "drawing", "services"],
   })
 }
