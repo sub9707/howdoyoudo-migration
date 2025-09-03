df1c056da4677343ea1e83ceadd9a547c0205684
김승섭 <sub9707@naver.com>
2025-09-04 00:45:44 +0900

    env 설정

--- src/app/contact/layout.tsx
+++ src/app/contact/layout.tsx
@@ -1,8 +1,9 @@
 import Script from 'next/script'
 import React from 'react'
-export const API = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.KAKAOJSKEY}&libraries=services,clusterer&autoload=false`
+export const API = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAOJSKEY}&libraries=services,clusterer&autoload=false`
 
 function Layout({ children }: { children: React.ReactNode }) {
+    console.log(process.env.NEXT_PUBLIC_KAKAOJSKEY)
     return (
         <>
             <Script
--- src/components/ui/useKakaoLoader.tsx
+++ src/components/ui/useKakaoLoader.tsx
@@ -1,8 +1,9 @@
 import { useKakaoLoader as useKakaoLoaderOrigin } from "react-kakao-maps-sdk"
 
 export default function useKakaoLoader() {
+  console.log(process.env.NEXT_PUBLIC_KAKAOJSKEY)
   useKakaoLoaderOrigin({
-    appkey: process.env.KAKAOJSKEY!,
+    appkey: process.env.NEXT_PUBLIC_KAKAOJSKEY!,
     libraries: ["clusterer", "drawing", "services"],
   })
 }
